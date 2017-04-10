d6d08e5b3dfab048eb1fb1f88af7f6e30eeb2628
Rajeev Kumar Singh <rajeevs@flock.co>
2017-04-10 11:52:38 +0530

    Classes and Evaluation Rules

--- classes/ClassDemo.scala
+++ classes/ClassDemo.scala
@@ -1,13 +1,15 @@
-class Calculator {
-	val brand: String = "HP"	
-	def add(m: Int, n: Int): Int = m + n
+class MyClass(x: Int, y: Int) {	// Defines a new type MyClass with a constructor
+	require(y > 0, "y must be positive")	// Precondition, triggering an IllegalArgumentException
+	def this(x: Int) = this(x, 1)	// auxiliary constructor
+	def nb1 = x		// public method, computed every time it is called
+	def nb2 = y
+	private def test(a: Int): Int = ???	// private method
+	val nb3 = x + y		// Computed only once
+	override def toString = x + ", " + y	// overriden method
 }
 
 object ClassDemo {
 	def main(args: Array[String]): Unit = {
-		val calc = new Calculator
-		println(calc)
-		println(calc.brand)
-		println(calc.add(2,3))
+		new MyClass(1,2) // created a new object of type
 	}
 }
